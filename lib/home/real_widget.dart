6a5a294fb304fad3f76bcf69c98f786cf1314117
Burak Akten <burak.akten@profe.com.tr>
2023-02-11 18:27:18 +0300

    Added a feature that chages the photos when double clicking the small photo.

--- lib/home/real_widget.dart
+++ lib/home/real_widget.dart
@@ -27,8 +27,8 @@ class _RealWidgetState extends ConsumerState<RealWidget> with SingleTickerProvid
 
   final double _margin = 10;
 
-  late File _selfie;
-  late File _photo;
+  late File _smallPhoto;
+  late File _bigPhoto;
 
   @override
   void initState() {
@@ -41,8 +41,7 @@ class _RealWidgetState extends ConsumerState<RealWidget> with SingleTickerProvid
       });
     });
 
-    _selfie = File(widget.selfie);
-    _photo = File(widget.photo);
+    _setPhotos(small: widget.selfie, big: widget.photo);
   }
 
   @override
@@ -51,15 +50,28 @@ class _RealWidgetState extends ConsumerState<RealWidget> with SingleTickerProvid
     super.dispose();
   }
 
+  void _setPhotos({required String small, required String big}) {
+    setState(() {
+      _smallPhoto = File(small);
+      _bigPhoto = File(big);
+    });
+  }
+
   Offset _getFinalOffset() {
     var width = _key.currentContext!.size!.width;
+    var height = _key.currentContext!.size!.height;
     var childWidth = _childKey.currentContext!.size!.width;
+    var childHeight = _childKey.currentContext!.size!.height;
 
     var finalWidth = _margin;
     var finalHeight = _margin;
 
     if (_offset.dx > width / 2) {
       finalWidth = width - childWidth - _margin;
+    }
+
+    if (_offset.dy >= height / 2) {
+      finalHeight = height - childHeight - _margin;
     }
 
     return Offset(finalWidth, finalHeight);
@@ -90,62 +102,71 @@ class _RealWidgetState extends ConsumerState<RealWidget> with SingleTickerProvid
         key: _key,
         borderRadius: BorderRadius.circular(20),
         child: Container(
-            height: 550,
-            decoration: BoxDecoration(
-                image: DecorationImage(
+          height: 500,
+          decoration: BoxDecoration(
+            image: DecorationImage(
               fit: BoxFit.cover,
               alignment: FractionalOffset.center,
-              image: File(_photo.path).existsSync()
-                  ? FileImage(_photo)
+              image: File(_bigPhoto.path).existsSync()
+                  ? FileImage(_bigPhoto)
                   : defaultTargetPlatform == TargetPlatform.iOS || defaultTargetPlatform == TargetPlatform.macOS
-                      ? CachedNetworkImageProvider(widget.photo) as ImageProvider
-                      : NetworkImage(widget.photo),
-            )),
-            child: Stack(
-              children: [
-                Positioned(
-                    left: _offset.dx,
-                    top: _offset.dy,
-                    child: GestureDetector(
-                      onTap: () {},
-                      onPanDown: (details) {
-                        _animationController.stop();
-                        ref.read(scrollBehaviorProvider.notifier).enable(false);
-                      },
-                      onPanUpdate: (details) {
-                        setState(() {
-                          _offset = Offset(_offset.dx + details.delta.dx, _offset.dy + details.delta.dy);
-                        });
-                      },
-                      onPanEnd: (details) {
-                        _runAnimation();
-                        ref.read(scrollBehaviorProvider.notifier).enable(true);
-                      },
-                      child: AbsorbPointer(
-                        child: Transform(
-                          transform: Matrix4.rotationY(math.pi),
-                          alignment: Alignment.center,
-                          child: Container(
-                              key: _childKey,
-                              width: 140,
-                              height: 190,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(20),
-                                  border: Border.all(color: Colors.black, width: 2),
-                                  image: DecorationImage(
-                                      fit: BoxFit.cover,
-                                      alignment: FractionalOffset.center,
-                                      image: _selfie.existsSync()
-                                          ? FileImage(_selfie)
-                                          : defaultTargetPlatform == TargetPlatform.iOS ||
-                                                  defaultTargetPlatform == TargetPlatform.macOS
-                                              ? CachedNetworkImageProvider(widget.selfie) as ImageProvider
-                                              : NetworkImage(widget.selfie)))),
+                      ? CachedNetworkImageProvider(_bigPhoto.path) as ImageProvider
+                      : NetworkImage(_bigPhoto.path),
+            ),
+          ),
+          child: Stack(
+            children: [
+              Positioned(
+                left: _offset.dx,
+                top: _offset.dy,
+                child: GestureDetector(
+                  onTap: () {},
+                  onDoubleTap: () {
+                    _setPhotos(small: _bigPhoto.path, big: _smallPhoto.path);
+                    ref.read(scrollBehaviorProvider.notifier).enable(true);
+                  },
+                  onPanDown: (details) {
+                    _animationController.stop();
+                    ref.read(scrollBehaviorProvider.notifier).enable(false);
+                  },
+                  onPanUpdate: (details) {
+                    setState(() {
+                      _offset = Offset(_offset.dx + details.delta.dx, _offset.dy + details.delta.dy);
+                    });
+                  },
+                  onPanEnd: (details) {
+                    _runAnimation();
+                    ref.read(scrollBehaviorProvider.notifier).enable(true);
+                  },
+                  child: AbsorbPointer(
+                    child: Transform(
+                      transform: Matrix4.rotationY(math.pi),
+                      alignment: Alignment.center,
+                      child: Container(
+                        key: _childKey,
+                        width: 140,
+                        height: 190,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          border: Border.all(color: Colors.black, width: 2),
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            alignment: FractionalOffset.center,
+                            image: _smallPhoto.existsSync()
+                                ? FileImage(_smallPhoto)
+                                : defaultTargetPlatform == TargetPlatform.iOS || defaultTargetPlatform == TargetPlatform.macOS
+                                    ? CachedNetworkImageProvider(_smallPhoto.path) as ImageProvider
+                                    : NetworkImage(_smallPhoto.path),
+                          ),
                         ),
                       ),
-                    ))
-              ],
-            )),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
